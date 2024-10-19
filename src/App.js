2418f9439b65d469b1a23504f184412c14901799
hyodogabriela <gahyodo@gmail.com>
2024-10-19 16:03:15 +0200

    Created new file Weather.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Weather from './Weather';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
     <h1>Weather Search App</h1>
+    <Weather />
     <footer>This project was coded by Gabriela Hyodo and is open-sourced at{" "}<a href="https://github.com/hyodogabriela/search-weather-project">GitHub</a></footer>
+    </div>
     </div>
   );
 }
