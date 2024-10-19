280089c68a1d4707d146031e1a8ea3d60d6ec7b8
hyodogabriela <gahyodo@gmail.com>
2024-10-19 15:49:44 +0200

    deleted unecessary text and code and added footer and h1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Weather Search App</h1>
+    <footer>This project was coded by Gabriela Hyodo and is open-sourced at{" "}<a href="https://github.com/hyodogabriela/search-weather-project">GitHub</a></footer>
+    </div>
+  );
+}
+
+export default App;
