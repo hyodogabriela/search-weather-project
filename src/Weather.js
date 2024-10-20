e185b9d775f4d1ab99b7836c9cb08fe6848e2826
hyodogabriela <gahyodo@gmail.com>
2024-10-20 13:34:19 +0200

    Created new component WeatherForecast and built the structure of the forecast

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
+import WeatherForecast from "./WeatherForecast";
 
 import WeatherInfo from "./WeatherInfo";
 
@@ -59,6 +60,7 @@ export default function Weather(props){
                     </div>
                 </form>
                 <WeatherInfo data={weatherData}/>
+                <WeatherForecast />
                 </div>
             );
 
---
+++ src/WeatherForecast.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast(){
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+            <div className="col">
+              <div className="WeatherForecast-day">
+               Thu
+               </div>
+               <div className="WeatherForecast-icon">
+               icon   
+               </div>  
+               <div className="WeatherForecast-temperatures">      
+               <span className="WeatherForecast-temperature-max">19º</span> 
+               <span className="WeatherForecast-temperature-min">10º</span>
+               </div>  
+            </div>
+        </div>
+      </div>  
+    );
+}
